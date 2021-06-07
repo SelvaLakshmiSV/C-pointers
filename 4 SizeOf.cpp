e02d0842b97fb74cff66d69bd4f2cca3aaa30c40
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-06-07 20:53:07 +0530

    SIZEOF()

---
+++ 4 SizeOf.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int dinky[5];
+    cout<<sizeof(5)<<endl;
+    cout<<sizeof(3.4)<<endl;
+    cout<<sizeof(78787878787878787878)<<endl;
+    cout<<sizeof('a')<<endl;
+    cout<<sizeof('abi')<<endl;
+    cout<<sizeof(dinky)<<endl;
+    return 0;
+}
